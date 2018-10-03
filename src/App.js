88b22772479962ea75effa346484d1fa9fa49da6
Alexander Dethloff <alexander_dethloff@Alexanders-MBP.fritz.box>
2018-10-03 17:20:06 +0200

    add settings pull from local storage

--- src/App.js
+++ src/App.js
@@ -26,16 +26,51 @@ const Bar = styled.div`
 `
 
 const Content = styled.div`
-
 `
+
+const checkFirstVisit = () => {
+  let CryptoDashData = localStorage.getItem('cryptoDash');
+  if(!CryptoDashData) {
+    return {
+      firstVisit: true,
+      page: 'settings'
+    };
+  }
+  return {};
+}
 
 class App extends Component {
   state = {
-    page: 'dashboard'
+    page: 'dashboard',
+    ...checkFirstVisit()
   }
 
   displayingDashboard = () => this.state.page === 'dashboard';
   displayingSettings = () => this.state.page === 'settings';
+  firstVisitMessage = () => {
+    if(this.state.firstVisit) {
+      return <div>Welcome to CryptoDash, please select your favorite coins to begin</div>
+    }
+  }
+
+  confirmFavorites = () => {
+    localStorage.setItem('cryptoDash', 'test');
+    this.setState({
+      firstVisit: false,
+      page: 'dashboard'
+    });
+  }
+
+  settingsContent = () => {
+    return (
+      <div>
+        {this.firstVisitMessage()}
+        <div onClick={this.confirmFavorites}>
+          Confirm Favorites
+        </div>
+      </div>
+    )
+  }
 
   render() {
     return (
@@ -45,12 +80,14 @@ class App extends Component {
             CryptoDash
           </Logo>
           <div></div>
-          <ControlButton 
-            onClick={() => {this.setState({page: 'dashboard'})}} 
-            active={this.displayingDashboard()}
-          >
-            Dashboard
-          </ControlButton>
+          {!this.state.firstVisit && (
+            <ControlButton 
+              onClick={() => {this.setState({page: 'dashboard'})}} 
+              active={this.displayingDashboard()}
+            >
+              Dashboard
+            </ControlButton>
+          )}
           <ControlButton 
             onClick={() => {this.setState({page: 'settings'})}} 
             active={this.displayingSettings()}
@@ -59,7 +96,7 @@ class App extends Component {
           </ControlButton>
         </Bar>
         <Content>
-          Hello I'm the {this.state.page}
+          {this.displayingSettings() && this.settingsContent()}
         </Content>
       </AppLayout>
       
